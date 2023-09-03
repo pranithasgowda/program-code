8608fa26dbf81c7b664bce99e21a595cb1e1f321
pranithasgowda <143249018+pranithasgowda@users.noreply.github.com>
2023-09-03 21:03:01 +0530

    Knapsack.java

--- Knapsack.java
+++ Knapsack.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Ppran
- */
-
+//java program to solve the knapsack problem using greedy technique
 import java.util.*;
 public class Knapsack
 {
